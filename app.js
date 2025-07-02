6d5ab18dd49d77ee5d63d4f17ea889e068ed5544
abdulsalamkz <abdulsalamkhanzada@gmail.com>
2025-07-02 22:30:56 +0500

    old work

--- app.js
+++ app.js
@@ -72,17 +72,87 @@
 // let result = getStudentName("Saad");
 // greet(result);
 
-function sayHello(name, callBack) {
-  setTimeout(() => {
-    const a = "Hello " + name;
-    console.log(a);
-    callBack();
-  }, 2000);
-}
+// function sayHello(name, callBack) {
+//   setTimeout(() => {
+//     const a = "Hello " + name;
+//     console.log(a);
+//     callBack();
+//   }, 2000);
+// }
 
-function greet() {
-  const a = "Loading";
-  console.log(a);
-}
+// function greet() {
+//   const a = "Loading";
+//   console.log(a);
+// }
 
-sayHello("Saad", greet);
+// sayHello("Saad", greet);
+
+// function process(name, callBack) {
+//   return callBack("Hello " + name);
+// }
+// function greet(message) {
+//   return message;
+// }
+
+// let result = process("Saad ", greet);
+
+// setTimeout(() => {
+//   console.log(result);
+// }, 2000);
+
+// function greet(name) {
+//   return "Hello " + name;
+// }
+
+// console.log(greet("Saad"));
+
+// function sayHello(name, callBack) {
+//   setTimeout(() => {
+//     const a = "Hello " + name;
+//     console.log(a);
+//     callBack();
+//   }, 2000);
+// }
+
+// function greet() {
+//   const a = "Loading";
+//   console.log(a);
+// }
+
+// sayHello("Saad", greet);
+
+// var ek function scope h is ko hum reassign or redeclare kar sakte hain
+// let ek block scope h is ko hum reassign kar sakte hain redelcare nhi kar sakte
+// const bhi block scope h is ko hum na hi reassign kar sakte hain or na hi redeclare
+
+// function do tarike k hote h ek arrow or ek sadha
+
+// function greet() {
+//   console.log("hello");
+// }
+// greet()
+// const name = () => {
+//   console.log("Saad");
+// };
+
+// function test(name, callBack) {
+//  setTimeout(() => {
+//    console.log("hello", name);
+//   callBack();
+//  }, 2000);
+// }
+// function greet() {
+//   console.log("Hi!");
+  
+// }
+
+// test("saad", greet);
+
+
+
+// if(true){
+// var a =10
+// let b =21
+// }
+
+// console.log(a );
